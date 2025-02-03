4a4a7d346953140f427cb66808bc882ef6781d22
gudu0 <125727026+Gudu0@users.noreply.github.com>
2025-02-03 00:00:41 -0800

    Update script.js
    reverted last change, made 4th value in rect() 300

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 
 ctx.beginPath();
-ctx.rect(40, 40, 50, 50);
+ctx.rect(20, 40, 50, 300);
 ctx.fillStyle = "#FF0000";
 ctx.fill();
 ctx.closePath();
