3349e2d6d228f28d50a8d1263a9e32cc84367048
gudu0 <125727026+Gudu0@users.noreply.github.com>
2025-02-03 00:08:09 -0800

    Update script.js
    replaced values with 20, 40, 50, 50

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 
 ctx.beginPath();
-ctx.rect(0, 0, 50, 300);
+ctx.rect(20, 40, 50, 50);
 ctx.fillStyle = "#FF0000";
 ctx.fill();
 ctx.closePath();
