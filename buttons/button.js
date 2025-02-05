55408bab5fd537c0b80c57bf038fd1019bba8d4f
gudu0 <125727026+Gudu0@users.noreply.github.com>
2025-02-04 22:37:12 -0800

    Update button.js
    added a new line to set the innerHTML of progval id element to progressbar value

--- buttons/button.js
+++ buttons/button.js
@@ -2,9 +2,11 @@ document.getElementById("testButton").addEventListener("click", function() {
   increase();
 });
 let progressbar = document.getElementById("testProgress");
+let progval = document.getElementById("progval");
 
 function increase() {
   progressbar.value = progressbar.value += 1;
+  progval.innerHTML = progressbar.value;
   if (progressbar.value == progressbar.max + 1) {
     progressbar.value = progressbar.max - progressbar.max;
   }
