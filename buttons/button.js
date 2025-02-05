d9a2287fe0c33a995e55e7650f5de04b69c8ac64
gudu0 <125727026+Gudu0@users.noreply.github.com>
2025-02-04 23:00:31 -0800

    Update button.js
    moved the progval setter into the timeout line

--- buttons/button.js
+++ buttons/button.js
@@ -9,8 +9,7 @@ function increase() {
   progval.innerHTML = progressbar.value;
   if (progressbar.value == progressbar.max) {
     //new code from google search to add a slight delay to setting the value to 0, might change it later, I don't like it.
-    setTimeout(() => { progressbar.value = progressbar.max - progressbar.max; }, 100);
-    progval.innerHTML = progressbar.value;
+    setTimeout(() => { progressbar.value = progressbar.max - progressbar.max; progval.innerHTML = progressbar.value; }, 100);
     //progressbar.value = progressbar.max - progressbar.max;
   }
 }
