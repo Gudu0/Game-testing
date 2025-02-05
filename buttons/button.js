7f51735f557a4c0391add2ba4f0b627dac5176a3
gudu0 <125727026+Gudu0@users.noreply.github.com>
2025-02-04 22:17:42 -0800

    Update button.js
    added some code to hopefully make the progress bar go up when clicking the button

--- buttons/button.js
+++ buttons/button.js
@@ -1,1 +1,8 @@
+document.getElementById("testButton").addEventListener("click", function() {
+  increase();
+});
+let progressbar = document.getElementById("testProgress");
 
+function increase() {
+  progressbar.value = progressbar.value += 1;
+}
