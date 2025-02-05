e4b402d6cb171b4e02d4dba3fcff209f39aefd6b
gudu0 <125727026+Gudu0@users.noreply.github.com>
2025-02-05 11:13:32 -0800

    Update button.js
    missed an e

--- buttons/button.js
+++ buttons/button.js
@@ -8,7 +8,7 @@ document.getElementById("button2").addEventListener("click", function() {
 let res1 = 0
 let res2 = 0
 let val1 = document.getElementById("val1");
-let val2 = document.getElemntById("val2");
+let val2 = document.getElementById("val2");
 
 val1.innerHTML = res1;
 val2.innerHTML = res2;
