91f1703eed00f95a3fc60e0172ad1de25b3d0f81
gudu0 <125727026+Gudu0@users.noreply.github.com>
2025-02-05 16:28:05 -0800

    Update button.js
    changed what the second button does, now requires 10 of resource one to make resource 2

--- buttons/button.js
+++ buttons/button.js
@@ -2,7 +2,7 @@ document.getElementById("testButton").addEventListener("click", function() {
   increase();
 });
 document.getElementById("button2").addEventListener("click", function() {
-  secondfunc();
+  tier2();
 });
 
 let res1 = 0
@@ -14,10 +14,16 @@ val1.innerHTML = res1;
 val2.innerHTML = res2;
 
 function increase() {
-  res1++
+  res1++;
   val1.innerHTML = res1;
 }
-function secondfunc() {
-  res2++
-  val2.innerHTML = res2;
+function tier2() {
+  if(res1 >= 10){
+    res1 - 10;
+    res2++;
+    val2.innerHTML = res2;
+  else{
+    
+  }
+  }
 }
