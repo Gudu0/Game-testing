4d01dc28d915e32df84bf84fe9b6344e6d76fd92
gudu0 <125727026+Gudu0@users.noreply.github.com>
2025-02-05 22:47:38 -0800

    Update button.js
    added some code in an else to make but2 red if clicked when not useable

--- buttons/button.js
+++ buttons/button.js
@@ -9,6 +9,8 @@ let res1 = 0
 let res2 = 0
 let val1 = document.getElementById("val1");
 let val2 = document.getElementById("val2");
+let but1 = document.getElementById("testButton";
+let but2 = document.getElementById("button2";
 
 val1.innerHTML = res1;
 val2.innerHTML = res2;
@@ -23,5 +25,8 @@ function tier2() {
     val1.innerHTML = res1;
     res2++;
     val2.innerHTML = res2;
+  else {
+    but2.style.backgroundColor = "red";
+  }
   }
 }
