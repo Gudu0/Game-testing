b7f9c2e3d8e9aad831c82998f84628ff42d351e6
gudu0 <125727026+Gudu0@users.noreply.github.com>
2025-02-06 01:31:29 -0800

    Update button.js
    removed some code

--- buttons/button.js
+++ buttons/button.js
@@ -7,10 +7,9 @@ document.getElementById("button2").addEventListener("click", function() {
 
 let res1 = 0
 let res2 = 0
+
 let val1 = document.getElementById("val1");
 let val2 = document.getElementById("val2");
-let but1 = document.getElementById("button1";
-let but2 = document.getElementById("button2";
 
 val1.innerHTML = res1;
 val2.innerHTML = res2;
