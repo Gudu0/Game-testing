a3f3fd81381b82cdf166f6728cfb4d1f4f9b66db
gudu0 <125727026+Gudu0@users.noreply.github.com>
2025-02-06 08:18:00 -0800

    Update button.js
    addded code for 3rd button

--- buttons/button.js
+++ buttons/button.js
@@ -1,28 +1,42 @@
 document.getElementById("button1").addEventListener("click", function() {
-  increase();
+  t1();
 });
 document.getElementById("button2").addEventListener("click", function() {
-  tier2();
+  t2();
+});
+document.getElementById("button3").addEventListener("click", function() {
+  t3();
 });
 
-let res1 = 0
-let res2 = 0
+let res1 = 0;
+let res2 = 0;
+let res3 = 0;
 
 let val1 = document.getElementById("val1");
 let val2 = document.getElementById("val2");
+let val3 = document.getElementById("val3");
 
 val1.innerHTML = res1;
 val2.innerHTML = res2;
+val3.innerHTML = res3;
 
-function increase() {
+function t1() {
   res1++;
   val1.innerHTML = res1;
 }
-function tier2() {
+function t2() {
   if(res1 >= 10){
     res1 -= 10;
     val1.innerHTML = res1;
     res2++;
     val2.innerHTML = res2;
+  }
+}
+function t3(){
+  if (res2>=10){
+    res2 -= 10;
+    val2.innerHTML = res2;
+    res3++;
+    val3.innerHTML = res3;
   }
 }
