9fcf463ba0d8a7945d4f038c4847f89a87bbac1c
gudu0 <125727026+Gudu0@users.noreply.github.com>
2025-02-07 09:15:19 -0800

    Update button.js
    commented out code

--- buttons/button.js
+++ buttons/button.js
@@ -15,9 +15,9 @@ document.getElementById("button2").addEventListener("click", function() {
 document.getElementById("button3").addEventListener("click", function() {
   t3();
 });
-document.getElementById("clrsav").addEventListener("click", function() {
-  clrsav();
-});
+// document.getElementById("clrsav").addEventListener("click", function() {
+//   clrsav();
+// });
 // document.getElementById("clearbut").addEventListener("click", clrsav());
 
 document.addEventListener("keydown", function(event){
