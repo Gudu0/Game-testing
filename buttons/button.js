6ca476becdac63e34c0967af28a2a047a99301cc
gudu0 <125727026+Gudu0@users.noreply.github.com>
2025-02-07 01:04:20 -0800

    Update button.js
    restoring old version, latest wasnt working.

--- buttons/button.js
+++ buttons/button.js
@@ -1,31 +1,12 @@
-//variable stuff
-//---------------------------------------------------------------------------------------------------------------------------------------------------------------------
-//---------------------------------------------------------------------------------------------------------------------------------------------------------------------
-//loads/creates save data
-//fetching past save data
-// if (localStorage.getItem("visited") == "true"){
-//   let res1 = Number(localStorage.getItem("res1"));
-//   let res1 = Number(localStorage.getItem("res1"));
-//   let res3 = Number(localStorage.getItem("res3"));
-// } else {
-//   //creating save data
-//   localStorage.setItem("visited", "true");
-  let res1 = 0;
-  let res2 = 0;
-  let res3 = 0;
-//   localStorage.setItem("res1", res1);
-//   localStorage.setItem("res2", res2);
-//   localStorage.setItem("res3", res3);
-// }
-// //calls the save function every 5 seconds
-// setInterval(save, 5000);
-
-//the 3 buttons
-document.getElementById("button1").addEventListener("click", t1());
-document.getElementById("button2").addEventListener("click", t2());
-document.getElementById("button3").addEventListener("click", t3());
-document.getElementById("clearbut").addEventListener("click", clrsav());
-
+document.getElementById("button1").addEventListener("click", function() {
+  t1();
+});
+document.getElementById("button2").addEventListener("click", function() {
+  t2();
+});
+document.getElementById("button3").addEventListener("click", function() {
+  t3();
+});
 document.addEventListener("keydown", function(event){
   if (event.shiftKey == true){
     shift = "true";
@@ -37,6 +18,11 @@ document.addEventListener("keyup", function(event){
   }
 });
 
+
+let res1 = 0;
+let res2 = 0;
+let res3 = 0;
+
 let val1 = document.getElementById("val1");
 let val2 = document.getElementById("val2");
 let val3 = document.getElementById("val3");
@@ -46,14 +32,11 @@ val2.innerHTML = res2;
 val3.innerHTML = res3;
 
 let shift = "false";
-//---------------------------------------------------------------------------------------------------------------------------------------------------------------------
-//---------------------------------------------------------------------------------------------------------------------------------------------------------------------
-//functions
+
 function t1() {
   res1++;
   val1.innerHTML = res1;
 }
-
 function t2() {
   if(res1 >= 10 || shift == "true"){
     if (shift == "true"){
@@ -68,7 +51,6 @@ function t2() {
     }
   }
 }
-
 function t3(){
   if (res2>=10 || shift == "true"){
     if (shift == "true"){
@@ -83,13 +65,3 @@ function t3(){
     }
   }
 }
-
-// function clrsav(){
-//   localStorage.clear();
-// }
-//saving function, saves the resource values into local storage
-// function save(){
-//   localStorage.setItem("res1", res1);
-//   localStorage.setItem("res2", res2);
-//   localStorage.setItem("res3", res3);
-// }
