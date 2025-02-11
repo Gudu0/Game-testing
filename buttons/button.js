64d2ec8d79344aa1b52a8e74ffa05c0d389c1c3a
gudu0 <125727026+Gudu0@users.noreply.github.com>
2025-02-11 15:41:05 -0800

    Update button.js
    fixed formattting

--- buttons/button.js
+++ buttons/button.js
@@ -41,14 +41,14 @@ document.getElementById("buydensegen").addEventListener("click", function() {
 });
 
 //key listeners
-document.addEventListener("keydown", function(event){
-  if (event.shiftKey == true){
-	shift = "true";
+document.addEventListener("keydown", function(event) {
+  if (event.shiftKey == true) {
+    shift = "true";
   }
 });
-document.addEventListener("keyup", function(event){
-  if (event.shiftKey == false){
-	shift = "false";
+document.addEventListener("keyup", function(event) {
+  if (event.shiftKey == false) {
+    shift = "false";
   }
 });
 
@@ -74,97 +74,103 @@ function t1() {
 }
 
 function t2() {
-  if(wood >= 9 || shift == "true"){
-  	if (shift == "true"){
-    	val1.innerHTML = wood;
-    	sturdyWood++;
-    	val2.innerHTML = sturdyWood;
-  	} else {
-			wood -= 9;
-    	val1.innerHTML = wood;
-    	sturdyWood++;
-    	val2.innerHTML = sturdyWood;
-		}
+  if (wood >= 9 || shift == "true") {
+    if (shift == "true") {
+      val1.innerHTML = wood;
+      sturdyWood++;
+      val2.innerHTML = sturdyWood;
+    } else {
+      wood -= 9;
+      val1.innerHTML = wood;
+      sturdyWood++;
+      val2.innerHTML = sturdyWood;
+    }
   }
 }
 
-function t3(){
-  if (sturdyWood>=9 || shift == "true"){
-  	if (shift == "true"){
-    	val2.innerHTML = sturdyWood;
-    	denseWood++;
-    	val3.innerHTML = denseWood;
-  	} else {
-			sturdyWood -= 9;
-    	val2.innerHTML = sturdyWood;
-    	denseWood++;
-    	val3.innerHTML = denseWood;
-		}
+function t3() {
+  if (sturdyWood >= 9 || shift == "true") {
+    if (shift == "true") {
+      val2.innerHTML = sturdyWood;
+      denseWood++;
+      val3.innerHTML = denseWood;
+    } else {
+      sturdyWood -= 9;
+      val2.innerHTML = sturdyWood;
+      denseWood++;
+      val3.innerHTML = denseWood;
+    }
   }
 }
 //idea for making file smaller, use string concatenation on variable names becauae I use the same scheme on all of them.
-function woodGen(){
-	if (woodGenCB.checked == true && woodgenbought == true){
-		WoodGenIntervalON = setInterval(t1_1, 1000);
-	} else {
-		clearInterval(WoodGenIntervalON);
-	}
+function woodGen() {
+  if (woodGenCB.checked == true && woodgenbought == true) {
+    WoodGenIntervalON = setInterval(t1_1, 1000);
+  } else {
+    clearInterval(WoodGenIntervalON);
+  }
 }
-function t1_1(){
+
+function t1_1() {
   wood++;
   val1.innerHTML = wood;
 }
-function sturdyGen(){
-	if (sturdyGenCB.checked == true && sturdygenbought == true){
-		SturdyGenIntervalON = setInterval(t2_1, 1000);
-	} else {
-		clearInterval(SturdyGenIntervalON);
-	}
+
+function sturdyGen() {
+  if (sturdyGenCB.checked == true && sturdygenbought == true) {
+    SturdyGenIntervalON = setInterval(t2_1, 1000);
+  } else {
+    clearInterval(SturdyGenIntervalON);
+  }
 }
-function t2_1(){
-	sturdyWood++;
-	val2.innerHTML = sturdyWood;
+
+function t2_1() {
+  sturdyWood++;
+  val2.innerHTML = sturdyWood;
 }
-function denseGen(){
-	if (denseGenCB.checked == true && densegenbought == true){
-		DenseGenIntervalON = setInterval(t3_1, 1000);
-	} else {
-		clearInterval(DenseGenIntervalON);
-	}
+
+function denseGen() {
+  if (denseGenCB.checked == true && densegenbought == true) {
+    DenseGenIntervalON = setInterval(t3_1, 1000);
+  } else {
+    clearInterval(DenseGenIntervalON);
+  }
 }
-function t3_1(){
-	denseWood++;
+
+function t3_1() {
+  denseWood++;
   val3.innerHTML = denseWood;
 }
-function buyWoodGen(){
- if (wood >= 10){
-		wood -= 10;
-		woodgenbought = true;
-		woodGenCB.checked = true;
-		woodGen()
-	  buywoodgen.remove();
-	 	woodGenCB.style.display = "inline-block";
-	}
+
+function buyWoodGen() {
+  if (wood >= 10) {
+    wood -= 10;
+    woodgenbought = true;
+    woodGenCB.checked = true;
+    woodGen()
+    buywoodgen.remove();
+    woodGenCB.style.display = "inline-block";
+  }
 }
 
-function buySturdyGen(){
-	if (sturdyWood >= 10){
-		sturdyWood -= 10;
-		sturdygenbought = true;
-		sturdyGenCB.checked = true;
-		sturdyGen();
-		buysturdygen.remove();
-		sturdygenCB.style.display = "inline-block";
-	}
+function buySturdyGen() {
+  if (sturdyWood >= 10) {
+    sturdyWood -= 10;
+    sturdygenbought = true;
+    sturdyGenCB.checked = true;
+    sturdyGen();
+    buysturdygen.remove();
+    sturdygenCB.style.display = "inline-block";
+  }
 }
 
-function buyDenseGen(){
-	if (denseWood >= 10){
-		denseWood -= 10;
-		densegenbought = true;
-		denseGenCB.checked = true;
-		denseGen();
-		buydensegen.remove();
-		densegenCB.style.display = "inline-block";
-	}
+function buyDenseGen() {
+  if (denseWood >= 10) {
+    denseWood -= 10;
+    densegenbought = true;
+    denseGenCB.checked = true;
+    denseGen();
+    buydensegen.remove();
+    densegenCB.style.display = "inline-block";
+  }
 }
